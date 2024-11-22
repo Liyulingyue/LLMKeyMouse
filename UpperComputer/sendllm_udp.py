51764dba2186bfad5ff5cbd42c23ad60c8bcb7a5
Ruby141 <baiyun141@163.com>
2024-11-22 14:18:06 +0800

    更新LLMPrompt,指令抽出json，发送指令为commands_str

--- UpperComputer/sendllm_udp.py
+++ UpperComputer/sendllm_udp.py
@@ -7,8 +7,8 @@ def main():
     server_ip = '192.168.1.12'
     server_port = 5000
 
-    model_name = "ernie-4.0"
-    access_token = "api_key"
+    model_name = "ernie-bot-4"
+    access_token = 'api_key'
 
     # 创建 Ernie_TransChain
     ernie_trans_chain = Ernie_TransChain(model_name=model_name, access_token=access_token)
@@ -17,16 +17,17 @@ def main():
 
     while True:
         # 获取用户输入
+        #鼠标移动到左上方，每次移动 5 和 10，共计 20 次，然后键盘输入你好，Python!
         message = input("Enter message to send: ")
         if message.lower() == 'exit':
             break
 
         # few-shot
-        few_shot = "输出示例为：('input','向右移动10个','trans_result','move(x=10)')"
-        response, success = ernie_trans_chain.run(message, few_shot)
+        # few_shot = "输出示例为：('input','向右移动10个','trans_result','move(x=10)')"
+        commands, success = ernie_trans_chain.run(message)
         if success:
             # 通过 UDP 发送数据
-            data = response.encode('utf-8')
+            data = commands.encode('utf-8')
             udp_socket.sendto(data, (server_ip, server_port))
             print(f"Sent {len(data)} bytes to {server_ip}:{server_port}")
         else:
